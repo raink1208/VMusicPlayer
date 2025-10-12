ea587ef162b275156ed869a80297b6cf08d2ed00
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-10-13 02:09:39 +0900

    エントリーポイント追加

---
+++ VMusicPlayerServer/src/main/kotlin/com/github/rain1208/vmusicplayerserver/VMusicPlayerServerApplication.kt
@@ -0,0 +1,11 @@
+package com.github.rain1208.vmusicplayerserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VMusicPlayerServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<VMusicPlayerServerApplication>(*args)
+}
