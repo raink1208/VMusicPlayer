5b3e36a94f32e7e421e373d1d839753eab4af7e1
raink1208 <58714901+raink1208@users.noreply.github.com>
2026-01-26 01:02:56 +0900

    CORSの修正

--- VMusicPlayerServer/src/main/kotlin/com/github/rain1208/vmusicplayerserver/config/WebConfig.kt
+++ VMusicPlayerServer/src/main/kotlin/com/github/rain1208/vmusicplayerserver/config/WebConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOriginPatterns("http://localhost:3000", "http://localhost:*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
